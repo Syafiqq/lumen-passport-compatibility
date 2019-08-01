bbba972533c804d9beb8563d6d11929af2b01b0d
Syafiqq <syafiq.rezpector@gmail.com>
2019-08-01 17:09:24 +0700

    updating passport

--- src/Passport.php
+++ src/Passport.php
@@ -3,8 +3,36 @@
 
 namespace Syafiqq\Passport;
 
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
+use Laravel\Lumen\Routing\Router as Router;
+
 
 class Passport extends \Laravel\Passport\Passport
 {
+    /**
+     * Binds the Passport routes into the controller.
+     *
+     * @param  callable|null  $callback
+     * @param  array  $options
+     * @return void
+     */
+    public static function routes($callback = null, array $options = [])
+    {
+        $callback = $callback ?: function ($router) {
+            $router->all();
+        };
 
+        $defaultOptions = [
+            'prefix' => 'oauth',
+            'namespace' => '\Laravel\Passport\Http\Controllers',
+        ];
+
+        $options = array_merge($defaultOptions, $options);
+
+        Route::group(Arr::except($options, ['namespace']), function (Router $router) use ($callback, $options) {
+            $routes = new RouteRegistrar($router, $options);
+            $routes->all();
+        });
+    }
 }
